e8778c67da31c962bb189cc669fa30042918944e
Himanshu <Himanshu6124@gmail.com>
2024-08-05 13:29:37 +0530

    added scroll

--- app/src/main/java/com/himanshu/pokemonapp/ui/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/himanshu/pokemonapp/ui/pokemondetail/PokemonDetailScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
@@ -86,7 +87,15 @@ fun PokemonDetailScreen(pokemonId: Int, viewModel: PokemonDetailViewModel = hilt
                             .fillMaxSize()
                             .padding(paddingValues)
                     ) {
-                        PokemonDetailView(pokemonDetail = detail)
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(10.dp)
+                        ) {
+                            item {
+                                PokemonDetailView(pokemonDetail = detail)
+                            }
+                        }
                     }
                 }
             }
